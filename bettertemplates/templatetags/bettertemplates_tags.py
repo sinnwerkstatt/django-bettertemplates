142b8fdb7f50edeed44a1d7682af036e98596819
Andreas Nüßlein <andreas@nuessle.in>
2018-08-14 12:27:59 +0200

    python 3, django 1.11

--- bettertemplates/templatetags/bettertemplates_tags.py
+++ bettertemplates/templatetags/bettertemplates_tags.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django import template
 from django.conf import settings
 from django.template.base import Template
@@ -16,11 +13,11 @@ register = template.Library()
 @register.tag(name='includeblock')
 def do_include_block(parser, token):
     """
-        Works like the {% include <template_name> %} django templatetag, 
-        but additionally allows for definition of inline blocks that can be 
+        Works like the {% include <template_name> %} django templatetag,
+        but additionally allows for definition of inline blocks that can be
         referenced in the included template.
         Usage:
-            
+
             {% includeblock 'example/includable_template.html' <...> %}
                 {% block myblock %}
                     <p>An inner block. Reference this using {{ myblock }} in the included template!</p>
@@ -30,18 +27,18 @@ def do_include_block(parser, token):
     """
     # inherit behaviour form ``include`` templatetag
     include_node = do_include(parser, token)
-    
-    # we give the parser a new "context" of blocks encountered in the inner includeblock, 
+
+    # we give the parser a new "context" of blocks encountered in the inner includeblock,
     # so duplicate blocks don't cause a TemplateSyntaxError
     loaded_blocks = None
     if hasattr(parser, '__loaded_blocks'):
         loaded_blocks = copy(parser.__loaded_blocks)
         parser.__loaded_blocks = []
     nodelist = parser.parse(('endincludeblock',))
-    
+
     if loaded_blocks is not None:
         parser.__loaded_blocks = loaded_blocks
-    
+
     parser.delete_first_token()
     return IncludeBlockNode(nodelist, include_node)
 
@@ -49,16 +46,16 @@ def do_include_block(parser, token):
 class IncludeBlockNode(template.Node):
     """ The {% includeblock <template> ... %} tag works just like an {% include ... %} templatetag,
         with the addition of allowing an inner block of content to be specified. The tag
-        allows all <with> kwargs supplied to the tag, just like the {% include ... %} tag. 
-    
+        allows all <with> kwargs supplied to the tag, just like the {% include ... %} tag.
+
         We render all nodes found inside the {% includeblock <template> %} Tag.
         Nodes found inside a {% block %} block are rendered into a context variable with the name
-            of their block. They can be used in the included <template> by accessing the variable 
+            of their block. They can be used in the included <template> by accessing the variable
             with the name of that block.
-        Nodes found outside {% block %} blocks are rendered together and output after the last node 
+        Nodes found outside {% block %} blocks are rendered together and output after the last node
             in the specified rendered <template>.
-    """ 
-    
+    """
+
     def __init__(self, nodelist, include_node):
         self.nodelist = nodelist
         self.include_node = include_node
@@ -71,20 +68,21 @@ class IncludeBlockNode(template.Node):
             #     self.template as Template
             if not hasattr(self.include_node, 'template'):
                 self.include_node.template = self.include_node.template_name
-            if not isinstance(self.include_node.template, Template) and callable(getattr(self.include_node.template, 'resolve', None)): 
+            if not isinstance(self.include_node.template, Template) and callable(
+                    getattr(self.include_node.template, 'resolve', None)):
                 # django >= 1.7
                 self.include_node.template = self.include_node.template.resolve(context)
                 if not callable(getattr(self.include_node.template, 'render', None)):
                     # If not, we'll try get_template
                     self.include_node.template = get_template(self.include_node.template)
             values = dict([(name, var.resolve(context)) for name, var
-                       in six.iteritems(self.include_node.extra_context)])
-            
+                           in six.iteritems(self.include_node.extra_context)])
+
             if self.include_node.isolated_context:
                 context = context.new(values)
             else:
                 context.update(values)
-                
+
             # render each named block in the inner block into a context variable
             block_list = self.nodelist.get_nodes_by_type(BlockNode)
             while block_list:
@@ -95,16 +93,15 @@ class IncludeBlockNode(template.Node):
                 # just rendered, block B will have been removed from the nodelist after rendering
                 block_list = self.nodelist.get_nodes_by_type(BlockNode)
             context.update(values)
-            
+
             # render the included template
-            output = self.include_node.template.render(context)
-                
+            output = self.include_node.template.render(context.flatten())
+
             if not self.include_node.isolated_context:
                 context.pop()
-            
+
             return output
         except:
             if settings.DEBUG:
                 raise
             return ''
-        
